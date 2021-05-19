69f14a328395ad15f4affbd43c656cfcb3203369
TuLapRonan <ronan18@live.nl>
2021-05-19 16:55:56 +0200

    simulated heat loss end caps and end temperature depending on flow rate pump

--- findFlowrate.m
+++ findFlowrate.m
@@ -1,8 +1,8 @@
 clc;
 clear all;
 
+%% Find end temperature heat vessel based on flow rate - uses flow.m which is a copy of the main model. Flow.m needs to be updated if the main model is changed.
 flowTable(1,:) = [0.1:0.1:3];
-
 for a = flowTable
     Column = round((1/0.1)*a);
     flowTable(2,Column) = flow(a);
